fed120cfc31100e7c785a82e02e41a856908e6a5
Hell0Ankit <ankitbohra660@gmail.com>
2026-03-15 22:48:36 +0530

    Setup Tailwind CSS

---
+++ tailwind_css/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "../templates/**/*.html",
+    "../**/templates/**/*.html",
+    "../static/**/*.js",
+    "../static/**/*.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
