837ba342183d036d09f1218fb56f3bdc7c40dc30
Hell0Ankit <ankitbohra660@gmail.com>
2026-03-17 10:46:30 +0530

    add file

--- accounts/views.py
+++ accounts/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'accounts/login.html')
+
+def signup(request):
+    return render(request, 'accounts/signup.html')
